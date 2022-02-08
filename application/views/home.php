3a879130b071b51ab9f98e25977f52e923bdf9e2
Jordan Correa <jordan.correa.0606@gmail.com>
2022-02-08 20:27:58 -0300

    Projeto mCI3

---
+++ application/views/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Projeto</title>
+    </head>
+    <body>
+        <?php echo $this->session->userdata('logado')->nomeUsuario; ?>
+         | <a class="btn btn-primary" href="<?php echo base_url(); ?>login/sair">Sair</a>
+        <h1>Home</h1>
+        <p>Olá mundo!</p>
+        <a href="<?php echo base_url() . 'pessoa'; ?>">Cadastro Pessoa</a> | 
+        <a href="<?php echo base_url() . 'usuario'; ?>">Cadastro Usuário</a>
+    </body>
+</html>
